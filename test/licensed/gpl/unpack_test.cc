a408cc894939c9cc1549128a0bd626f65492eb13
Adam Hooper <adam@adamhooper.com>
2010-12-19 23:27:07 -0500

    Replace "Foobar" from GPL headers

--- test/licensed/gpl/unpack_test.cc
+++ test/licensed/gpl/unpack_test.cc
@@ -3,18 +3,19 @@
  *
  * This file is part of refinery (GPL portion).
  *
- * Foobar is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * refinery (GPL portion) is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Foobar is distributed in the hope that it will be useful,
+ * refinery (GPL portion) is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ * along with refinery (GPL portion).  If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 
 #include <gtest/gtest.h>
