976ec4b393aa2d99d54150db917be73952e279a3
Adam Hooper <adam@adamhooper.com>
2011-02-20 02:36:47 -0500

    Doxygen-ize camera.h

--- include/refinery/camera.h
+++ include/refinery/camera.h
@@ -5,74 +5,127 @@ namespace refinery {
 
 class ExifData;
 
-/*
- * A Camera holds static data for an actual camera model.
+/**
+ * Holds static data about an actual camera model.
  *
  * Each camera model behaves slightly differently: different compression, for
- * instance, or different color profiles. Each camera model is represented by
- * a class which derives from Camera.
+ * instance, or different color profiles. That's what Camera represents. Most
+ * of the time users really want CameraData, which uses a Camera to make sense
+ * of Exif data.
  *
  * Some Camera methods accept an ExifData& as a parameter. That's
  * because different cameras treat the same Exif tags differently.
  */
 class Camera {
 public:
-  /*
-   * Every camera has a different notion of R, G, B, C, Y, M, or K. This table
-   * maps the camera's RGB or CMYK to XYZ.
+  /**
+   * These values are used in mapping from sensors to RGB, CMYK and XYZ.
    */
   struct ColorConversionData {
-    short black;
-    short maximum;
-    double xyzToCamera[4][3];
-    double rgbToCamera[4][3];
-    double cameraToRgb[3][4];
-    double cameraToXyz[3][4];
-    double cameraMultipliers[4];
-    double scalingMultipliers[4];
+    short black; /**< Black level, the camera's conceptual 0. */
+    short maximum; /**< Maximum sensor value. */
+    double xyzToCamera[4][3]; /**< Matrix to convert XYZ to sensor values. */
+    double rgbToCamera[4][3]; /**< Matrix to convert sRGB to sensor values. */
+    double cameraToRgb[3][4]; /**< Matrix to convert sensor values to sRGB. */
+    double cameraToXyz[3][4]; /**< Matrix to convert sensor values to XYZ. */
+    double cameraMultipliers[4]; /**< Multipliers for making other matrices. */
+    double scalingMultipliers[4]; /**< To scale sensor values for max 65535. */
   };
 
-  virtual const char* name() const = 0;
+  /**
+   * The make of the camera, for example "NIKON".
+   */
   virtual const char* make() const = 0;
+
+  /**
+   * The model of the camera, for example "D5000".
+   */
   virtual const char* model() const = 0;
+
+  /**
+   * The make + model of the camera, for example "NIKON D5000".
+   */
+  virtual const char* name() const = 0;
+
+  /**
+   * The number of sensor colors, typically 3 or 4.
+   */
   virtual unsigned int colors() const = 0;
+
+  /**
+   * Some multipliers for changing color spaces.
+   */
   virtual ColorConversionData colorConversionData() const = 0;
+
+  /**
+   * The Exif "Orientation", from 1 to 8, saying how one image is flipped.
+   */
   virtual unsigned int orientation(const ExifData& exifData) const = 0;
+
+  /**
+   * Returns \c true if this is the right Camera for exifData.
+   */
   virtual bool canHandle(const ExifData& exifData) const = 0;
 };
 
-/*
- * A Camera& coupled with an ExifData&.
+/**
+ * A Camera coupled with one photo's ExifData.
  *
  * This is the public face that callers care about. Callers shouldn't need to
  * know whether a default color profile comes hard-coded or embedded in Exif.
  * This class abstracts everything.
  *
- * CamerData instances are lightweight. Shallow-copy them at will.
+ * CameraData instances are lightweight. Shallow-copy them at will.
  */
 class CameraData {
   const Camera& mCamera;
   const ExifData& mExifData;
 
 public:
+  /**
+   * Creates a CameraData for a given photo.
+   *
+   * The Camera and ExifData are both passed by reference and must exist for
+   * the lifetime of this CameraData.
+   */
   CameraData(const Camera& camera, const ExifData& exifData)
       : mCamera(camera), mExifData(exifData) {}
+
+  /**
+   * Copy constructor.
+   *
+   * The Camera and ExifData are both passed by reference.
+   */
   CameraData(const CameraData& rhs)
       : mCamera(rhs.camera()), mExifData(rhs.exifData()) {}
 
+  /**
+   * The Camera object.
+   */
   const Camera& camera() const { return mCamera; }
+
+  /**
+   * The ExifData object particular to this photo.
+   */
   const ExifData& exifData() const { return mExifData; }
 
-  /* Returns the image orientation, a number from 1 to 8 (see TIFF docs) */
+  /**
+   * The Exif "Orientation", from 1 to 8, saying how one image is flipped.
+   */
   unsigned int orientation() const;
 
-  /* Returns the number of colors in the image */
+  /**
+   * The number of sensor colors, typically 3 or 4.
+   */
   unsigned int colors() const;
 
+  /**
+   * Some multipliers for changing color spaces.
+   */
   Camera::ColorConversionData colorConversionData() const;
 };
 
-/*
+/**
  * Returns Camera instances.
  *
  * Call CameraFactory::instance().detectCamera(exifData) to return a Camera
@@ -91,9 +144,12 @@ private:
   ~CameraFactory();
 
 public:
+  /**
+   * The global CameraFactory.
+   */
   static CameraFactory& instance();
 
-  /*
+  /**
    * Figures out which const Camera& applies to the given Exif data, and
    * returns it.
    *
@@ -107,16 +163,31 @@ public:
   const Camera& detectCamera(const ExifData& exifData) const;
 };
 
-/*
+/**
  * Returns CameraData instances.
  *
- * Each photograph corresponds with one CameraData instance. Only the Exif data
- * is necessary to determine the photograph's properties.
+ * Example usage:
+ *
+ * \code
+ * std::auto_ptr<refinery::ExifData> exifData(somehowFindExifData());
+ *
+ * CameraData cameraData = CameraDataFactory::instance().getCameraData(*exifData);
+ *
+ * std::cout << "Photo orientation: " << cameraData.orientation() << std::endl;
+ * \endcode
  */
 class CameraDataFactory {
 public:
+  /**
+   * The global CameraDataFactory.
+   */
   static CameraDataFactory& instance();
 
+  /**
+   * Returns a CameraData object which helps explain a photo's properties.
+   *
+   * The ExifData must exist for the lifetime of the returned CameraData.
+   */
   CameraData getCameraData(const ExifData& exifData) const;
 };
 
